0f350d4c7888b4ded706011f4ed97bc08b542be5
tomconversion <123917926+tomconversion@users.noreply.github.com>
2023-03-07 14:26:38 +1100

    Contentful Redirect in place

--- next.config.mjs
+++ next.config.mjs
@@ -27,62 +27,28 @@ export default nextConfig
 
 async function collectRedirectMap(){
   console.log("next.config.mjs sourcing redirects");
-  const redirects = await fetchRedirects();
-  console.log("next.config.mjs found the following redirects > ", redirects.allRedirect.edges);
-  const redirectMap = [];
-  redirects.allRedirect.edges.forEach((redirect) => {
-      if(redirect?.node?.source && redirect?.node?.destination?.url)
-      {
-        let destination = redirect?.node?.destination?.url;
-        if(destination){
-          destination = destination.replace("/homepage", "");
-        }
-        if(redirect?.node?.destination?.superAlias && redirect?.node?.destination?.superAlias !== ""){
-          destination = redirect?.node?.destination?.superAlias;
-        }       
-        console.log("next.config.mjs redirecting > ", redirect.node.source, " to > ", destination, " isPermanent > ", redirect.node.isPermanent);
-        redirectMap.push({
-          source: redirect.node.source,
-          destination: destination,
-          permanent: redirect.node.isPermanent,
-        });
-    }
-  });
+  const redirects = await fetchRedirects();  
+
+  const cmsVariant = process.env.NEXT_PUBLIC_CMS_VARIANT;
+  let redirectMap = [];
+
+  if(cmsVariant === 'contentful') {
+    redirectMap = collectContentfulRedirectMap(redirects);
+  }else if(cmsVariant === 'kontent') {
+
+  }else if(cmsVariant === 'heartcore') {
+    redirectMap = collectUmbracoRedirectMap(redirects);
+  }
+  
+
   return redirectMap;
 }
 
 async function fetchRedirects() {
-  const query = `
-  {
-    allRedirect{
-        edges{
-            node{
-                source
-                destination {
-                    url
-                    level
-                    name
-                    ... on Homepage {
-                      superAlias
-                    }
-                    ... on DynamicPage {
-                      superAlias
-                    }
-                    ... on Landing {
-                      superAlias
-                    }
-                }
-                name
-                isPermanent
-            }
-        }
-    }    
-  } 
-  `;
+
+  const query = getQuery();
 
   const cmsVariant = process.env.NEXT_PUBLIC_CMS_VARIANT;
-
-
 
   const headers = {
         'Content-Type': 'application/json'
@@ -134,4 +100,149 @@ export async function fetchAPI(
   } else {
     return json
   }
+}
+
+function getQuery(){
+  let query = `
+  {
+    allRedirect{
+        edges{
+            node{
+                source
+                destination {
+                    url
+                    level
+                    name
+                    ... on Homepage {
+                      superAlias
+                    }
+                    ... on DynamicPage {
+                      superAlias
+                    }
+                    ... on Landing {
+                      superAlias
+                    }
+                }
+                name
+                isPermanent
+            }
+        }
+    }    
+  } 
+  `;
+
+  const cmsVariant = process.env.NEXT_PUBLIC_CMS_VARIANT;
+
+  if(cmsVariant === 'kontent') {
+    query = `
+    {
+      allRedirect{
+          edges{
+              node{
+                  source
+                  destination {
+                      url
+                      level
+                      name
+                      ... on Homepage {
+                        superAlias
+                      }
+                      ... on DynamicPage {
+                        superAlias
+                      }
+                      ... on Landing {
+                        superAlias
+                      }
+                  }
+                  name
+                  isPermanent
+              }
+          }
+      }    
+    } 
+    `;
+  } else if(cmsVariant === 'contentful') {
+    query = `
+      query{
+        redirectCollection{
+            items{
+                isPermanent
+                source
+                destination{
+                    __typename
+                    ... on Page{
+                        slug
+                        parent{
+                            ... on Page{
+                                slug  
+                                parent{
+                                    ... on Page{
+                                        slug                               
+                                    }
+                                }                             
+                            }
+                        }                                  
+                    }
+                }
+            }
+        }
+    }
+    `;
+  }
+ return query;
+}
+
+function collectUmbracoRedirectMap(redirects){
+  const redirectMap = [];
+  redirects.allRedirect.edges.forEach((redirect) => {
+    if(redirect?.node?.source && redirect?.node?.destination?.url)
+    {
+      let destination = redirect?.node?.destination?.url;
+      if(destination){
+        destination = destination.replace("/homepage", "");
+      }
+      if(redirect?.node?.destination?.superAlias && redirect?.node?.destination?.superAlias !== ""){
+        destination = redirect?.node?.destination?.superAlias;
+      }       
+      console.log("next.config.mjs redirecting > ", redirect.node.source, " to > ", destination, " isPermanent > ", redirect.node.isPermanent);
+      redirectMap.push({
+        source: redirect.node.source,
+        destination: destination,
+        permanent: redirect.node.isPermanent,
+      });
+    }
+  });
+  return redirectMap;
+}
+
+function collectContentfulRedirectMap(redirects) {
+  const redirectMap = [];
+  redirects.redirectCollection.items.forEach((redirect) => {
+    if (redirect?.source && redirect?.destination?.slug) {
+      let destination = redirect?.destination?.slug;
+      
+      // check if destination slug starts with '/'
+      if (destination.charAt(0) !== '/') {
+        let parentSlug = redirect?.destination?.parent;
+        let currentParent = redirect?.destination?.parent;
+
+        // add the final parent slug to the front of destination slug
+        destination = `${parentSlug.slug}/${destination}`;
+
+        // loop through parent levels until we find one that starts with '/'
+        while (currentParent?.parent && currentParent?.parent?.slug.charAt(0) !== '/') {
+          currentParent = currentParent?.parent;
+          destination = `${currentParent.slug}/${destination}`;   
+        }
+      }
+
+      console.log("next.config.mjs redirecting > ", redirect.source, " to > ", destination, " isPermanent > ", redirect.isPermanent);
+      redirectMap.push({
+        source: redirect.source,
+        destination: destination,
+        permanent: redirect.isPermanent,
+      });
+    }
+  });
+  return redirectMap;
 }

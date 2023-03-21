8bff6cb7e25d57696483dc5964f6244f055fdda8
Jeevan Kumar <g1mishra.dev@gmail.com>
2023-03-21 11:31:25 +0530

    Stories-Section Heartcore query added

---
+++ sites/landify/graphql/heartcore/stories/stories.ts
@@ -0,0 +1,94 @@
+import {
+  StoriesCardProps,
+  StoriesSectionProps,
+} from "@/ui-base/components/ui/sections/stories-section"
+import { variablesMultiSiteByIdentifier } from "@/ui-base/lib/cms/_base/tools/common/multiSite"
+import { LanguageSite, PageIdentifier } from "@/ui-base/lib/cms/constants"
+
+export function stories(pageIdentifier: PageIdentifier) {
+  return `query HomepageComponentsBySlug($slug: String!) {
+    homepage(url: $slug) {
+      slug: url
+      name
+      children(where: { contentTypeAlias_ends_with: "dataFolder" }) {
+        edges {
+          node {
+            __typename
+            contentTypeAlias
+            children(where: { contentTypeAlias_ends_with: "StoriesSection" }) {
+              edges {
+                node {
+                  name
+                  ... on StoriesSection {
+                    __typename
+                    title
+                    description
+                    cards {
+                      ... on StoriesCard{
+                        text
+                        text1
+                        text2
+                        image{
+                          media {
+                            url
+                            name
+                          }
+                        }
+                        image1{
+                          media {
+                            url
+                            name
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }`
+}
+
+export function variables(
+  pageIdentifier: PageIdentifier,
+  languageSite: LanguageSite
+) {
+  return variablesMultiSiteByIdentifier(pageIdentifier, languageSite)
+}
+
+export default function GetourClientQuery() {
+  return stories
+}
+
+export function mapStoriesData(data: any): StoriesSectionProps {
+
+  const stories = data.homepage.children.edges[0].node.children.edges
+
+  const storiesSection = stories.find(
+    (item: any) => item.node.__typename === "StoriesSection"
+  )?.node
+
+  // console.log("storiesSection", JSON.stringify(storiesSection, null, 2))
+
+
+  const cards: StoriesCardProps[] = storiesSection?.cards?.map(
+    (card: any) => ({
+      text: card.text,
+      text1: card.text1,
+      text2: card.text2,
+      image_src: card.image.media.url,
+      image_alt: card.image.media.name,
+      image_src1: card.image1.media.url,
+      image_alt1: card.image1.media.name,
+    })
+  )
+  return {
+    title: storiesSection.title,
+    description: storiesSection.description,
+    cards,
+  }
+}
--- sites/landify/graphql/kontent/stories/stories.ts
+++ sites/landify/graphql/kontent/stories/stories.ts
@@ -45,70 +45,10 @@ export default function GetourClientQuery() {
   return stories
 }
 
-// {
-//   homepage: {
-//     bodyItems: {
-//       totalCount: 4,
-//       items: [
-//         {},
-//         {},
-//         {},
-//         {
-//           __typename: "StoriesSection",
-//           title: "Real Stories from Real Customers",
-//           description: "Get inspired by these stories.",
-//           cards: {
-//             items: [
-//               {
-//                 text: "To quickly start my startup landing page design, I was looking for a landing page UI Kit. Landify is one of the best landing page UI kit I have come across. It’s so flexible, well organised and easily editable.",
-//                 text1: "Floyd Miles",
-//                 text2: "Vice President, GoPro",
-//                 image: {
-//                   url: "https://assets-au-01.kc-usercontent.com:443/42bb4688-b828-0269-2832-a1f8434e1cee/f870c36a-2168-4f5e-a45b-fd6086b0c74f/Hubspot.svg",
-//                   description: "HubSpot"
-//                 },
-//                 image1: {
-//                   url: "https://assets-au-01.kc-usercontent.com:443/42bb4688-b828-0269-2832-a1f8434e1cee/94f60643-f8b6-4d00-9fe9-4b0ffb2a4a55/quote-mark.svg",
-//                   description: "quote"
-//                 }
-//               },
-//               {
-//                 text: "Landify saved our time in designing my company page.",
-//                 text1: "Kristin Watson",
-//                 text2: "Co-Founder, FedEx",
-//                 image: {
-//                   url: "https://assets-au-01.kc-usercontent.com:443/42bb4688-b828-0269-2832-a1f8434e1cee/25e5178d-c3d7-4382-8e55-38bedf4f4152/FedEx.svg",
-//                   description: "FedEX"
-//                 },
-//                 image1: {
-//                   url: "https://assets-au-01.kc-usercontent.com:443/42bb4688-b828-0269-2832-a1f8434e1cee/94f60643-f8b6-4d00-9fe9-4b0ffb2a4a55/quote-mark.svg",
-//                   description: "quote"
-//                 }
-//               },
-//               {
-//                 text: "I used Landify and created a landing page for my startup within a week. The Landify UI Kit is simple and highly intuitive, so anyone can use it.",
-//                 text1: "Jane Cooper",
-//                 text2: "CEO, Airbnb",
-//                 image: {
-//                   url: "https://assets-au-01.kc-usercontent.com:443/42bb4688-b828-0269-2832-a1f8434e1cee/3f915d48-2ca7-4953-a684-e085b757b2b6/Airbnb.svg",
-//                   description: "Airbnb"
-//                 },
-//                 image1: {
-//                   url: "https://assets-au-01.kc-usercontent.com:443/42bb4688-b828-0269-2832-a1f8434e1cee/94f60643-f8b6-4d00-9fe9-4b0ffb2a4a55/quote-mark.svg",
-//                   description: "quote"
-//                 }
-//               }
-//             ]
-//           }
-//         }
-//       ]
-//     }
-//   }
-// }
 
 export function mapStoriesData(data: any): StoriesSectionProps {
   const stories = data.homepage.bodyItems.items
-  console.log("stories", stories);
+  // console.log("stories", stories);
   
   const storiesSection = stories.find(
     (item: any) => item.__typename === "StoriesSection"

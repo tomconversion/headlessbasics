9745dab7b196c1a34ba3d1f211de317981e436aa
Thomas Tyack <thomas.tyack@conversiondigital.com.au>
2023-03-21 12:01:55 +1100

    WIP dynamic site

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,49 @@
 import { GetLanguageSiteByCode, GetMainSiteLanguage } from '@/ui-base/lib/cms/heartcore/tools/urlTools';
-import { buildPageData } from '@/ui-base/lib/services/graphqlDataService'
-// import { IndexPage as Homepage } from '../sites/multisite/pages/Index'
+import { buildPageData } from '@/ui-base/lib/services/graphqlDataService';
 import dynamic from 'next/dynamic';
 import { GetSite } from '@/ui-base/lib/services/siteContextService';
+import fetch from 'isomorphic-unfetch';
 
 interface Props {
-  data?: any
+  data?: any;
+  indexPath: string;
 }
+
+// const Homepage = dynamic<Props>(
+//   async () => {
+//     const siteName = GetSite().name;
+//     const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/getSiteIndexPage?siteName=${siteName}`);
+
+//     if (res.ok) {
+//       const { path } = await res.json();
+//       return import(path).then((mod) => mod.IndexPage);
+//     } else {
+//       throw new Error(await res.text());
+//     }
+//   },
+//   { ssr: true },
+// );
 
 const Homepage = dynamic<Props>(
-  () => {
-    const name = `../sites/${GetSite().name}/pages/IndexPage`
-    return import(name).then((mod) => mod.IndexPage);
+  ({ indexPath }) => {
+    return import(indexPath).then((mod) => mod.IndexPage);
   },
-  { ssr: true }
+  { ssr: true },
 );
 
-export default function IndexPage({ data }) {
-  return <Homepage data={data} />
+export default function IndexPage({ data, indexPath }) {
+  return <Homepage data={data} indexPath={indexPath} />;
 }
 
-export async function getStaticProps({ params }) {
+export async function getServerSideProps({ req }) {
+  const data = await buildPageData('home', false, GetLanguageSiteByCode(GetMainSiteLanguage()), { slug: '' });
+  const siteName = GetSite().name;
+  const indexPath = `../sites/${siteName}/pages/IndexPage`;
 
-  const data = await buildPageData("home", false, GetLanguageSiteByCode(GetMainSiteLanguage()), {slug: ""}); 
-  
   return {
     props: {
-      data
+      data,
+      indexPath,
     },
-    // Next.js will attempt to re-generate the page:
-    // - When a request comes in
-    // - At most once every 10 seconds
-    revalidate: 100, // In seconds
-  }
+  };
 }
